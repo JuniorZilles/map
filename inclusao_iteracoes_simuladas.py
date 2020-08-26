fd9af21002719e13cfd7b10b2319b78228a2c2f4
Junior Zilles <junior_zilles@hotmail.com>
2020-08-26 11:42:42 -0300

    fixes e inicio do calculo do MAP

--- inclusao_iteracoes_simuladas.py
+++ inclusao_iteracoes_simuladas.py
@@ -4,6 +4,7 @@ import os
 import json
 import datetime
 import random
+import requests
 
 def update_single_document(client, index, id, jsondata: str):
     try:
@@ -30,6 +31,24 @@ def index_single_document(client, index, id, jsondata: str):
     except ElasticsearchException as e:
         raise Exception(
             "Elasticsearch -> index_single_document -> exception error:" + str(e) + "\n entrada: "+jsondata)
+
+def get_consult(query: str, method: str, limit:str, offset:str, documents_id: list):
+    jsondata = json.dumps({
+        'query': query,
+        'method': method,
+        'limit': limit,
+        'offset': offset,
+        'documents': documents_id
+    })
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
+            'content-type': 'application/json'
+        }
+    )
+    response = requests.post(url='http://172.17.0.2:31227/search', data=jsondata, headers = headers)
+    return response.json()
 
 
 def get_document(client, index, id):
@@ -66,55 +85,60 @@ methods = [
 
 querys = ['borges', 'WELTON', 'PAPA', 'American', 'Batteries', 'levantamento', 'energia']
 operacao = ['visualizou', 'baixou xfdf', 'baixou jpg']
-documents = ['ed863078-463c-4302-bd6a-e6ef7db89778', 
-'e770e245-5688-422b-8965-1fc181f3edc8',
-'b60b729d-fb42-413c-8c0f-66d0402e1b8a', 
-'52949a92-948d-4cbc-b5d2-aafef3569f12', 
-'37c5f60e-b044-4fa9-8582-f2e115af2501', 
-'9cee72fd-835a-440b-a09b-a2a2c4777509', 
-'6b597709-ced2-42d9-b899-ef9d983785de', 
-'dbaa0eab-345c-405e-b3c4-d5311234b202', 
-'09227de0-9017-4a14-bb4a-d46f053ee49e', 
-'5e194287-117a-4db3-bd75-39c25fdad19a', 
-'7bdfcea9-34d5-4fe5-9662-1d02d6d4de27', 
-'5ffbe027-4c61-4651-bd6c-8308705e69f7', 
-'009d00ce-0375-47c9-9843-accfdd03f3dc', 
-'3c55fcee-ed0f-4b32-bd50-b197585b467e',
-'0da8e1df-c5df-4a9c-b66a-7f5173d8ac44']
+documents = ["ed863078-463c-4302-bd6a-e6ef7db89778", 
+"e770e245-5688-422b-8965-1fc181f3edc8",
+"b60b729d-fb42-413c-8c0f-66d0402e1b8a", 
+"52949a92-948d-4cbc-b5d2-aafef3569f12", 
+"37c5f60e-b044-4fa9-8582-f2e115af2501", 
+"9cee72fd-835a-440b-a09b-a2a2c4777509", 
+"6b597709-ced2-42d9-b899-ef9d983785de", 
+"dbaa0eab-345c-405e-b3c4-d5311234b202", 
+"09227de0-9017-4a14-bb4a-d46f053ee49e", 
+"5e194287-117a-4db3-bd75-39c25fdad19a", 
+"7bdfcea9-34d5-4fe5-9662-1d02d6d4de27", 
+"5ffbe027-4c61-4651-bd6c-8308705e69f7", 
+"009d00ce-0375-47c9-9843-accfdd03f3dc", 
+"3c55fcee-ed0f-4b32-bd50-b197585b467e",
+"0da8e1df-c5df-4a9c-b66a-7f5173d8ac44"]
 
 for a in range(0,10):
   posmeth = random.randint(0, 6)
   posquer = random.randint(0, 6)
-  posoper = random.randint(0, 2)
-  posdoc = random.randint(0, 14)
-  posicao = random.randint(0, 20)
+  search = get_consult(querys[posquer], methods[posmeth], '20', '0', documents)
+  
   jsonstr = {
-      "query": querys[posquer], "metodo": methods[posmeth], "timestamp": datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),"limit":0, "offset":20, "interacao":[]
-  }
-  jsonstr1 = {
-      "script": {
-      "source": "ctx._source.interacao.add(params.interacao)",
-      "lang": "painless",
-      "params": {
-        "interacao": {"timestamp":datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), 'id_documento': documents[posdoc],"posicao":posicao, "operação":operacao[posoper]}
-      }
-    }
-  }
-  jsonstr2 = {
-      "script": {
-      "source": "ctx._source.offset = params.offset",
-      "lang": "painless",
-      "params": {
-        "offset": 40
-      }
-    }
+      "query": querys[posquer], "metodo": methods[posmeth], "timestamp": datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),"limit":20, "offset":0, "interacao":[]
   }
   strjson = json.dumps(jsonstr)
-  strjson1 = json.dumps(jsonstr1)
-  strjson2 = json.dumps(jsonstr2)
   index_single_document(client, index, a, strjson)
-  update_single_document(client, index, a, strjson1)
-  update_single_document(client, index, a, strjson2)
+  qtd = len(search['hits'])-1
+  for b in range(0, 10):
+    posicao = random.randint(0, qtd)
+    posoper = random.randint(0, 2)
+    jsonstr1 = {
+        "script": {
+        "source": "ctx._source.interacao.add(params.interacao)",
+        "lang": "painless",
+        "params": {
+          "interacao": {"timestamp":datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), 'id_documento': search['hits'][posicao]['file_id'],"posicao":posicao, "operação":operacao[posoper]}
+        }
+      }
+    }
+    # jsonstr2 = {
+    #     "script": {
+    #     "source": "ctx._source.offset = params.offset",
+    #     "lang": "painless",
+    #     "params": {
+    #       "offset": 40
+    #     }
+    #   }
+    # }
+    
+    strjson1 = json.dumps(jsonstr1)
+    # strjson2 = json.dumps(jsonstr2)
+    
+    update_single_document(client, index, a, strjson1)
+    # update_single_document(client, index, a, strjson2)
 #print(get_document(client, index, 3))
 
 get = {
--- main_j_tse.py
+++ main_j_tse.py
@@ -2,19 +2,43 @@ from elastic_client import Elastic_client
 import requests
 import time
 import json
+from metricas import *
+from model import *
+class Document:
+    def __init__(self, doc_id, posicao):
+        self.doc_id = doc_id
+        self.pos = posicao
+
+def found(lista, doc):
+    if len(lista) == 0:
+        return True
+    elif len(lista) > 0:
+        if lista[0].doc_id == doc.doc_id and lista[0].pos == doc.pos:
+            return False
+        else:
+            return found(lista[1:], doc)
 
 
-def get_documents_from_interactions(interactions: list):
+def get_documents_and_positions_from_interactions(interactions: list):
     documents_id = []
-    [documents_id.append(x['id_documento'])
-     for x in interactions if x['id_documento'] not in documents_id]
-    return documents_id
+    position = []
+    atual_pos = -1
+    doc_atual = ''
+    for x in interactions:
+        if x['id_documento'] not in documents_id:
+            documents_id.append(x['id_documento'])
+        doc = Document(x['id_documento'], x['posicao'])
+        if found(position, doc):
+            position.append(doc)
+    return documents_id, position
 
 
-def get_consult(query: str, method: str, documents_id: list):
+def get_consult(query: str, method: str, limit:int, offset:int, documents_id: list):
     jsonbody = json.dumps({
         'query': query,
         'method': method,
+        'limit': limit,
+        'offset': offset,
         'documents': documents_id
     })
     response = requests.post(url='http://172.17.0.2:31227/search', data=jsonbody, headers = {'content-type': 'application/json'})
@@ -33,17 +57,39 @@ def get_interactions():
     ]
     client = Elastic_client()
     result = client.get_documents()
-
+    times_search = []
+    iteration_list = []
     for res in result['hits']['hits']:
         query = res['_source']['query']
         method = res['_source']['metodo']
+        limit = res['_source']['limit']
+        offset = res['_source']['offset']
         timestamp = res['_source']['timestamp']
         interactions = res['_source']['interacao']
-        documents_id = get_documents_from_interactions(interactions)
-        search = get_consult(query, method, documents_id)
-        print(search)
+        interactionssorted = sorted(interactions, key=lambda k: k['posicao'])
+        documents_id, positions = get_documents_and_positions_from_interactions(interactionssorted)
+        inicio = time.time()
+        search = get_consult(query, method, limit, offset, documents_id)
+        tempo_gasto = time.time() - inicio
+        times_search.append(tempo_gasto)
+        qtd = search['hits_amount']
+        retrieved = []
+        relevant = []
+        pos = 0
+        count = 1
+        for x in search['hits']:
+            doc = Document(x['file_id'], pos)
+            retrieved.append(count)
+            if found(positions, doc):
+                relevant.append(count)
+            count += 1
+        qtd_retornado = str(len(retrieved))
+        avg = average_precision(retrieved, relevant)
 
-
+        recall_list =  recall_at_k(retrieved, relevant)
+        precision_list = precision_at_k(retrieved, relevant)
+        m = model(res, search, qtd_retornado, avg, relevant, precision_list, recall_list)
+        iteration_list.append(m)
 def main():
     inicio = time.time()
     get_interactions()
